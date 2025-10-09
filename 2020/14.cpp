05dd142f143eca4c453e02a6870b59e935741ae8
Jan Obrovnik <jo7261@student.uni-lj.si>
2025-10-09 08:47:48 +0200

    Dodatek: 2020/14 - part 2

--- 2020/14.cpp
+++ 2020/14.cpp
@@ -1,6 +1,8 @@
 /*
-	t ~ 1 ura
+	t ~ 1 h
 	lahko
+	t = 1:13:19 h
+	relativno lahko
 */
 
 #include <iostream>
@@ -62,7 +64,6 @@ std::map<int, std::bitset<36>> preberiPodatke(const std::string& pot) {
 	return resitev;
 }
 
-
 unsigned long long vsota(const std::map<int, std::bitset<36>>& podatki) {
 
 	unsigned long long resitev = 0;
@@ -73,13 +74,108 @@ unsigned long long vsota(const std::map<int, std::bitset<36>>& podatki) {
 	return resitev;
 }
 
+
+long long razberiMesto(std::string mestoBit, const int& moznost) {
+
+	const std::bitset<36> moznostBit(moznost);
+
+	int index = 0;
+	for (auto it = mestoBit.rbegin(); it != mestoBit.rend(); it++) {
+		if (*it == 'X') {
+			*it = moznostBit[index] + '0';
+			index++;
+		}
+	}
+
+	return std::stoll(mestoBit, NULL, 2);
+}
+void zapisiVSpomin(std::map<long long, long long>& spomin, 
+				   const std::string& maska, const long long& mesto, const long long& vrednost) {
+
+	std::string mestoBit = std::bitset<36>(mesto).to_string();
+
+	int index = 0;
+	for (const char& c : maska) {
+		if (c != '0')
+			mestoBit[index] = c;
+		index++;
+	}
+
+	const int& velikost = std::count(mestoBit.begin(), mestoBit.end(), 'X');
+	int steviloMoznosti = pow(2, velikost);
+
+	for (int i = 0; i < steviloMoznosti; i++) {
+
+		spomin[razberiMesto(mestoBit, i)] = vrednost;
+	}
+}
+
+std::map<long long, long long> preberiPodatke2(const std::string& pot) {
+
+	std::map<long long, long long> resitev;
+
+	std::fstream podatki;
+	podatki.open(pot, std::ios::in);
+
+	if (!podatki.is_open()) {
+		std::cout << "Datoteke \"" << pot << "\" ni bilo mogoce odpreti.\n";
+		return resitev;
+	}
+
+	std::string prepoznava;
+	std::string maska;
+	long long mesto;
+	long long vrednost;
+
+	while (podatki.peek() != EOF) {
+
+		podatki >> prepoznava;
+
+		if (prepoznava == "mask") {
+			podatki.ignore(2);
+			podatki >> maska;
+		}
+		else {
+			mesto = std::stoi(prepoznava.substr(4, prepoznava.size() - 5));
+			podatki.ignore(2);
+			podatki >> vrednost;
+
+			zapisiVSpomin(resitev, maska, mesto, vrednost);
+		}
+	}
+
+	return resitev;
+}
+
+unsigned long long vsota2(const std::map<long long, long long>& podatki) {
+
+	unsigned long long resitev = 0;
+	
+	for (const auto& i : podatki)
+		resitev += i.second;
+
+	return resitev;
+}
+
+
 int main() {
 
 	std::map<int, std::bitset<36>> podatki(preberiPodatke("2020/14.txt"));
 
 	unsigned long long resitev1 = vsota(podatki);
-	std::cout << "Vsota vseh vrednosti je " << resitev1 << ".\n";
+	std::cout << "Vsota vseh vrednosti po verziji 1.0. je " << resitev1 << ".\n";
 
+	std::map<long long, long long> podatki2(preberiPodatke2("2020/14.txt"));
+
+	unsigned long long resitev2 = vsota2(podatki2);
+	std::cout << "Vsota vseh vrednosti po verziji 2.0. je " << resitev2 << ".\n";
+
+	if (_MSVC_LANG == 202302L) std::cout << "C++23";
+	else if (_MSVC_LANG == 202002L) std::cout << "C++20";
+	else if (_MSVC_LANG == 201703L) std::cout << "C++17";
+	else if (_MSVC_LANG == 201402L) std::cout << "C++14";
+	else if (_MSVC_LANG == 201103L) std::cout << "C++11";
+	else if (_MSVC_LANG == 199711L) std::cout << "C++98";
 
 	return 0;
 }
